ad09f6b2c81f74cb2242b57bbd1e2700c0945d7f
Elias Fernandes <eliasneto0014@gmail.com>
2022-12-30 10:32:25 -0300

    add components

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,60 @@
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../context/ContextProvider";
+import axiosClient from "../axios-client.js";
+import {useEffect} from "react";
+import logo from "../logo.png";
+
+
+export default function DefaultLayout() {
+  const {user, token, setUser, setToken, notification} = useStateContext();
+
+  if (!token) {
+    return <Navigate to="/login"/>
+  }
+
+  const onLogout = ev => {
+    ev.preventDefault()
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({})
+        setToken(null)
+      })
+  }
+
+  useEffect(() => {
+    axiosClient.get('/user')
+      .then(({data}) => {
+         setUser(data)
+      })
+  }, [])
+
+  return (
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Usuarios</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            <img src={logo} style={{height: "80px",width:"120px"}} alt="logo" />
+          </div>
+
+          <div>
+            {user.name} &nbsp; &nbsp;
+            <a onClick={onLogout} className="btn-logout" href="#">Sair</a>
+          </div>
+        </header>
+        <main>
+          <Outlet/>
+        </main>
+        {notification &&
+          <div className="notification">
+            {notification}
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
