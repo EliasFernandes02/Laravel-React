3bddfb555c6754087729af870d6103ecabde475b
Elias Fernandes <eliasneto0014@gmail.com>
2022-12-30 10:02:48 -0300

    implementing react project

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
         $credentials = $request->validated();
         if (!Auth::attempt($credentials)) {
             return response([
-                'message' => 'Provided email or password is incorrect'
+                'message' => 'Email incorreto'
             ], 422);
         }
 
